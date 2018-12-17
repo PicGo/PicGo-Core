6bbe5c48256c685fb4b56f0540f9d8f13dee951e
Molunerfinn <marksz@teamsz.xyz>
2018-12-17 17:03:03 +0800

    Fixed: chose->choose

--- src/plugins/commander/index.ts
+++ src/plugins/commander/index.ts
@@ -3,12 +3,12 @@ import pluginHandler from './pluginHandler'
 import config from './config'
 import upload from './upload'
 import setting from './setting'
-import chose from './choose'
+import choose from './choose'
 
 export default (ctx: PicGo): void => {
   ctx.cmd.register('pluginHandler', pluginHandler)
   ctx.cmd.register('config', config)
   ctx.cmd.register('setting', setting)
   ctx.cmd.register('upload', upload)
-  ctx.cmd.register('chose', chose)
+  ctx.cmd.register('choose', choose)
 }
