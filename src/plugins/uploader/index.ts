96b2b3a6c22e647e3e97863d45c09905615f6103
Molunerfinn <marksz@teamsz.xyz>
2020-03-07 18:09:20 +0800

    :sparkles: Feature: remove weibo support

--- src/plugins/uploader/index.ts
+++ src/plugins/uploader/index.ts
@@ -1,7 +1,6 @@
 import PicGo from '../../core/PicGo'
 import SMMSUploader from './smms'
 import tcYunUploader from './tcyun'
-import weiboUploader from './weibo'
 import githubUploader from './github'
 import qiniuUploader from './qiniu'
 import imgurUploader from './imgur'
@@ -11,7 +10,6 @@ import upYunUploader from './upyun'
 export default (ctx: PicGo): void => {
   ctx.helper.uploader.register('smms', SMMSUploader)
   ctx.helper.uploader.register('tcyun', tcYunUploader)
-  ctx.helper.uploader.register('weibo', weiboUploader)
   ctx.helper.uploader.register('github', githubUploader)
   ctx.helper.uploader.register('qiniu', qiniuUploader)
   ctx.helper.uploader.register('imgur', imgurUploader)
