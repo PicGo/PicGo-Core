7e106558aa88995704b5ae0a9482f4656889760e
Molunerfinn <marksz@teamsz.xyz>
2020-03-07 18:06:04 +0800

    :sparkles: Feature: add smms-v2 support

--- src/plugins/uploader/smms.ts
+++ src/plugins/uploader/smms.ts
@@ -1,12 +1,14 @@
 import PicGo from '../../core/PicGo'
+import { PluginConfig } from '../../utils/interfaces'
 
-const postOptions = (fileName: string, image: Buffer): any => {
+const postOptions = (fileName: string, image: Buffer, apiToken: string): any => {
   return {
     method: 'POST',
-    url: `https://sm.ms/api/upload`,
+    url: 'https://sm.ms/api/v2/upload',
     headers: {
       contentType: 'multipart/form-data',
-      'User-Agent': 'PicGo'
+      'User-Agent': 'PicGo',
+      Authorization: apiToken
     },
     formData: {
       smfile: {
@@ -21,13 +23,17 @@ const postOptions = (fileName: string, image: Buffer): any => {
 }
 
 const handle = async (ctx: PicGo): Promise<PicGo> => {
+  const smmsConfig = ctx.getConfig('picBed.smms.token')
+  if (!smmsConfig) {
+    throw new Error('Can\'t find smms config, please provide api token, see https://sm.ms/home/apitoken')
+  }
   const imgList = ctx.output
   for (let i in imgList) {
     let image = imgList[i].buffer
     if (!image && imgList[i].base64Image) {
       image = Buffer.from(imgList[i].base64Image, 'base64')
     }
-    const postConfig = postOptions(imgList[i].fileName, image)
+    const postConfig = postOptions(imgList[i].fileName, image, smmsConfig)
     let body = await ctx.Request.request(postConfig)
     body = JSON.parse(body)
     if (body.code === 'success') {
@@ -49,7 +55,25 @@ const handle = async (ctx: PicGo): Promise<PicGo> => {
   return ctx
 }
 
+const config = (ctx: PicGo): PluginConfig[] => {
+  let userConfig = ctx.getConfig('picBed.smms')
+  if (!userConfig || typeof userConfig !== 'object') {
+    userConfig = {}
+  }
+  const config = [
+    {
+      name: 'token',
+      message: 'api token',
+      type: 'input',
+      default: userConfig.token || '',
+      required: true
+    }
+  ]
+  return config
+}
+
 export default {
   name: 'SM.MS图床',
-  handle
+  handle,
+  config
 }
